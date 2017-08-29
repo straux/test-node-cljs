f36258e208b216738518db81c94f01e2385b05d7
Stéphane Raux <stephane.raux@oscaro.com>
2017-08-29 12:10:12 +0200

    Print message parameter

--- src/test_node_cljs/core.cljs
+++ src/test_node_cljs/core.cljs
@@ -1,23 +1,25 @@
 (ns test-node-cljs.core
   (:require [clojure.string :as str]
             [functions.entry-points]
+            [goog.string :as gstring]
+            [goog.string.format]
             [cljs.nodejs :as nodejs]))
 
 ;; see https://cloud.google.com/functions/docs/writing/
 
 (nodejs/enable-util-print!)
-(. (nodejs/require "source-map-support") (install #js {:environment "node"})) 
+(. (nodejs/require "source-map-support") (install #js {:environment "node"}))
 
-(defn ^:export helloWorld 
+(defn ^:export helloWorld
   [req res]
   (let [msg (.. req -body -message)]
      (when (= msg "fail")
        (throw (new js/Error "failed")))
-     (. res (send "hello World !"))))
+     (. res (send (gstring/format "hello %s !" (or msg "World"))))))
 
 (defn -main [& args]
    (when (= (first args) "fail")
-     (throw (new js/Error "test"))) 
+     (throw (new js/Error "test")))
    (println args))
 
 (set! *main-cli-fn* -main)
